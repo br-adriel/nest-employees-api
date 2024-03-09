93d295859faf8ef30d77a7ad634f9c6e75238def
Adriel <adriel.fsantos@outlook.com>
2024-03-09 17:19:13 -0300

    feat: Update users dto with validators

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,13 @@
-import Role from 'src/types/Role';
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDTO {
+  @IsString({ message: '"name" deve ser uma string' })
+  @IsNotEmpty({ message: '"name" deve ser preenchido' })
   name: string;
+
+  @IsEmail({}, { message: '"email" deve ser um email válido' })
   email: string;
-  role: Role;
+
+  @IsEnum(['INTERN', 'ENGINEER', 'ADMIN'], { message: '"role" inválido' })
+  role: 'INTERN' | 'ENGINEER' | 'ADMIN';
 }
